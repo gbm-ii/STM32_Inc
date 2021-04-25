6b9fd17b91788ca9a314609873107c7bc718f208
gbm-ii <79805744+gbm-ii@users.noreply.github.com>
2021-04-25 19:53:27 +0200

    Update stm32nucleo144-l4.h
    LED masks and LPUART pins corrected

--- Boards/stm32nucleo144-l4.h
+++ Boards/stm32nucleo144-l4.h
@@ -1,15 +1,12 @@
 /*
  * Nucleo-144 L496 & L4R5 board defs
- * gbm 02'18
+ * gbm 02'18..04'21
  */
  
 #include "stm32l4yy.h"
 
 #define HSE_VALUE	8000000u
 
-#define LED_PORT	GPIOE	// LEDs on PE8..15
-#define LEDR1_BIT	9	// clockwise, top
-#define LEDO1_BIT	10
 // all LEDs active HIGH
 // LD1
 #define LEDG_PORT	GPIOC
@@ -20,10 +17,14 @@
 // LD3
 #define LEDR_PORT	GPIOB
 #define LEDR_BIT	14
+// USB OTG power switch with status LED
+#define LEDU_PORT	GPIOG
+#define LEDU_BIT	6
 
-#define LEDG_MSK	(1u << LEDG1_BIT)
-#define LEDB_MSK	(1u << LEDB1_BIT)
-#define LEDR_MSK	(1u << LEDR1_BIT)
+#define LEDG_MSK	(1u << LEDG_BIT)
+#define LEDB_MSK	(1u << LEDB_BIT)
+#define LEDR_MSK	(1u << LEDR_BIT)
+#define LEDU_MSK	(1u << LEDU_BIT)
 
 #define LEDG_DUTY	TIM3->CCR2	// or TIM8->CCR2
 #define LEDB_DUTY	TIM17->CCR1	// CH1N
@@ -34,6 +35,6 @@
 
 #define BTN_DOWN	(BTN_PORT->IDR >> BTN_BIT & 1)
 
-// GPIOA USART2 pins
-#define TX_BIT	2
-#define RX_BIT	3
+// GPIOG LPUART1 pins
+#define TX_BIT	7
+#define RX_BIT	8
